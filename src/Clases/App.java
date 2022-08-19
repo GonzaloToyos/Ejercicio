66caa6f5eba88f3c7a1896a097d35e6fb1590e1d
Gonzalo Toyos <gonzalotoyos2001@gmail.com>
2022-08-18 23:21:22 -0300

    Primer

--- src/Clases/App.java
+++ src/Clases/App.java
@@ -4,6 +4,14 @@ import javax.xml.stream.util.StreamReaderDelegate;
 public class App {
     public static void main(String[] args) throws Exception {
         Persona persona1 = new Persona();
+
+        persona1.setEdad(23);
+        persona1.setNombre("PABLO");
+        persona1.setTelefono("12345678");
+
+        System.out.println("Nombre: " + persona1.getNombre());
+        System.out.println("Edad: " + persona1.getEdad());
+        System.out.println("Telefono: " + persona1.getTelefono());
     }
 }
 
