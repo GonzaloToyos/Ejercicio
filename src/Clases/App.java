2a57bf20ce7bed13e4027a32c741e539ff98fd54
Gonzalo Toyos <gonzalotoyos2001@gmail.com>
2022-08-18 23:21:06 -0300

    Primer commit

---
+++ src/Clases/App.java
@@ -0,0 +1,9 @@
+package Clases;
+import javax.xml.stream.util.StreamReaderDelegate;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Persona persona1 = new Persona();
+    }
+}
+
